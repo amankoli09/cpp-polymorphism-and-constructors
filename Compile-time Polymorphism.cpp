9477effe72baf52216e69d0259467a8178d18937
Aman Koli <2025.amana@isu.ac.in>
2025-12-29 18:13:02 +0530

    Update Compile-time Polymorphism.cpp

--- Compile-time Polymorphism.cpp
+++ Compile-time Polymorphism.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 using namespace std;
 
-class Calculator {
+class Calculator
+{
 public:
-    Calculator() {
-        cout << "Calculator Ready\n";
+    Calculator()
+    {
+        cout << "This is the simple calculator" << endl;
     }
 
-    int add(int a, int b) {
+    int add(int a, int b)
+    {
         return a + b;
     }
 
-    double add(double a, double b) {
+    double add1(int a, int b)
+    {
         return a + b;
+    }
+
+    float add2(int a, int b)
+    {
+        return a + b;
+    }
+
+    int sub(int a, int b)
+    {
+        return a - b;
     }
 };
 
-int main() {
-    Calculator c;
-    cout << c.add(5, 3) << endl;
-    cout << c.add(2.5, 3.4) << endl;
+int main()
+{
+    Calculator obj1;
+    int x, y;
+
+    cout << "Enter the value of x: " << endl;
+    cin >> x;
+
+    cout << "Enter the value of y: " << endl;
+    cin >> y;
+
+    cout << "The addition of x and y is: " << obj1.add(x, y) << endl;
+    cout << "The double addition of x and y is: " << obj1.add1(x, y) << endl;
+    cout << "The float addition of x and y is: " << obj1.add2(x, y) << endl;
+    cout << "The subtraction of x and y is: " << obj1.sub(x, y) << endl;
+
+    return 0;
 }
